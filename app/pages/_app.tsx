7cf84814bd7bdb1ff117ce18cb7e8b3b57da0e3b
gange <749918694@qq.com>
2024-01-31 14:31:57 +0800

    添加统计的代码

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,12 +3,21 @@ import { ThemeProvider } from '~/lib/theme'
 import '../styles/globals.css'
 import '../styles/swiper.css'
 import React from "react";
+import Head from "next/head";
+
+declare global {
+    interface Window {
+        dataLayer: any[]; // 这里使用 'any' 类型，你也可以根据实际情况使用更具体的类型
+    }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+      <>
+          <ThemeProvider>
+              <Component {...pageProps} />
+          </ThemeProvider>
+      </>
   )
 }
 
